880e493e986d41ee0158b79883ee70f9c2b66d6d
RodriguezOswaldo <ownsan@byui.edu>
2019-05-23 16:24:29 -0400

    commit

--- updatecsswjs/scripts.js
+++ updatecsswjs/scripts.js
@@ -1,11 +1,16 @@
 let header = document.querySelector('.logo');
 let titles = document.getElementsByTagName('h2');
+let rightCol = document.querySelector('.rightcol img');
 
-        header.style.width = "220px";
-        header.style.background = "url('https://source.unsplash.com/random/240x240')";
-        header.style.zIndex = "-1";
+        header.style.cssText = `width="220px";
+                                background = "url('https://source.unsplash.com/random/240x240')";
+                            zIndex = "-1";`
         console.log(titles);
+        console.log(rightCol);
         for(i=0;i<titles.length;i++){
             titles[i].style.fontSize ="30px";
-            
         }
+    rightCol.style.cssText =`width: 440px;
+                              height: 440px; 
+                              border-radius: 100px 20px;
+                              box-shadow: 1px 10px 5px #888;`
