00dcccc689f5f3ececc24f100118ef3ac42214da
RodriguezOswaldo <ownsan@byui.edu>
2019-05-24 19:26:20 -0400

    commit

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -7,21 +7,26 @@ function theFunction() {
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', 'starWarsCast2.json', true);
     ourRequest.onload = function () {
-        var myData = JSON.parse(ourRequest.responseText);
-        var someContent = " ";
-        // newFunction(ourData);
-        for(i = 0; i < myData.length; i ++){
-            someContent = "<ul>" +  myData[i].title + "</ul>" + "<p> The Best Character in that movie was    " + myData[i].Casting.bestChar +"</p>";
-            theContainer[0].insertAdjacentHTML('beforeend',someContent);
+        if (ourRequest.status >= 200 && ourRequest.status < 400) {//Handling Exceptions
+            var myData = JSON.parse(ourRequest.responseText);
+            var someContent = " ";
+            // newFunction(ourData);
+            for (i = 0; i < myData.length; i++) {
+                someContent = "<ul>" + myData[i].title + "</ul>" + "<p> The Best Character in that movie was    " + myData[i].Casting.bestChar + "</p>";
+                theContainer[0].insertAdjacentHTML('beforeend', someContent);
+            }
+            console.log(myData);
+        } else {
+            someContent = "There was an error requesting the data";
+            theContainer[0].insertAdjacentElement('beforeend', someContent);
         }
-        console.log(myData);
     }
     ourRequest.send();
     clickCounter++;
-    if(clickCounter = 2){
+    if (clickCounter = 2) {
         btn[0].style.display = "none";
         theContainer[1].style.display = "none";
-    }else{
+    } else {
         console.log("Somethings is wrong with the Button!");
     }
 };
