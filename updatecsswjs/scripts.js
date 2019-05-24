93d639c55a1a141edccae4261814528d4c0be148
RodriguezOswaldo <ownsan@byui.edu>
2019-05-24 11:24:55 -0400

    commit

--- updatecsswjs/scripts.js
+++ updatecsswjs/scripts.js
@@ -1,21 +1,30 @@
-let header = document.querySelector('.logo');
-let titles = document.getElementsByTagName('h2');
-let rightCol = document.querySelector('.rightcol img');
+let heading = document.getElementsByTagName('h2');
+console.log(heading);
 let images = document.getElementsByTagName('img');
+console.log(images);
+let text = document.getElementsByTagName('p');
+console.log(p);
 
-console.log(titles);
-// alert(titles[0].nodeName);
+for(i=0;i<images.length; i++){
+        images[i].style.boxShadow = "20px 10px 15px #323";
+        images[i].style.borderRadius = "20px 130px";
+}
+for(i=0; i<heading.length; i++){
+        heading[i].style.fontSize = "30px";
+}
+// console.log(titles);
+// // alert(titles[0].nodeName);
 
-        header.style.cssText = `width="220px";
-                                background = "url('https://source.unsplash.com/random/240x240')";
-                                zIndex = "-2";`
-        console.log(titles);
-        console.log(rightCol);
-        for(i=0;i<titles.length;i++){
-            titles[i].style.cssText = `borderBottom = "3px dotted #333";
-                                         color = gray";`	
+//         header.style.cssText = `width="220px";
+//                                 background = "url('https://source.unsplash.com/random/240x240')";
+//                                 zIndex = "-2";`
+//         console.log(titles);
+//         console.log(rightCol);
+//         for(i=0;i<titles.length;i++){
+//             titles[i].style.cssText = `borderBottom = "3px dotted #333";
+//                                          color = gray";`	
 
-        }
+//         }
 //     rightCol.style.cssText =`width: 440px;
 //                               height: 440px; 
 //                               border-radius: 100px 20px;
