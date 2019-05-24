a4fa9dce35df9e698aa63816f20286d2da801e68
RodriguezOswaldo <ownsan@byui.edu>
2019-05-24 17:39:02 -0400

    commit

--- ajax/practice.js
+++ ajax/practice.js
@@ -5,37 +5,38 @@ var btn = document.getElementsByClassName('btn');
 
 btn[0].addEventListener("click", function(){
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'starWarsCast2.json', true);
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json', true);
     ourRequest.onload = function () {
         var ourData = JSON.parse(ourRequest.responseText);
         console.log(ourData[0]);
         newFunction(ourData);
 
-        //  document.getElementsByTagName('p').innerHTML = ourData[0];
     }
     ourRequest.send();
 });
 
-function theFunction() {
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'starWarsCast2.json', true);
-    ourRequest.onload = function () {
-        var ourData = JSON.parse(ourRequest.responseText);
-        var someContent = " ";
-        console.log(ourData[0]);
-        // newFunction(ourData);
-        for(i = 0; i < ourData.length; i ++){
-            someContent = "<ul>" +  ourData[i].title + "</ul>" + "<p> The Best Character in that movie was    " + ourData[i].Casting.bestChar +"</p>";
-            theContainer[0].insertAdjacentHTML('beforeend',someContent);
-        }
+// function theFunction() {
+//     var ourRequest = new XMLHttpRequest();
+//     ourRequest.open('GET', 'starWarsCast2.json', true);
+//     ourRequest.onload = function () {
+//         var ourData = JSON.parse(ourRequest.responseText);
+//         var someContent = " ";
+//         console.log(ourData[0]);
+//         // newFunction(ourData);
+//         for(i = 0; i < ourData.length; i ++){
+//             someContent = "<ul>" +  ourData[i].title + "</ul>" + "<p> The Best Character in that movie was    " + ourData[i].Casting.bestChar +"</p>";
+//             theContainer[0].insertAdjacentHTML('beforeend',someContent);
+//         }
 
-    }
-    ourRequest.send();
-};
+//     }
+//     ourRequest.send();
+// };
 
 function newFunction(theData){
     var someContent = " ";
     for(i=0; i<theData.length; i++){
+        someContent += "<ul>" +  theData[i].name + "is a " + theData[i].species +". </ul>";
+
     }
 
     theContainer[0].insertAdjacentHTML('beforeend',someContent);
