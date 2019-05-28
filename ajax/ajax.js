3358c23053909392074c9f95c2eec7023c78f59a
rodriguezoswaldo <owner2303@gmail.com>
2019-05-27 23:09:58 -0400

    commit

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,30 +1,30 @@
-var theContainer = document.getElementsByTagName('p');
-var btn = document.getElementsByClassName('btn');
-var clickCounter = 1;
+// var theContainer = document.getElementsByTagName('p');
+// var btn = document.getElementsByClassName('btn');
+// var clickCounter = 1;
 
 
-function theFunction() {
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'starWarsCast2.json', true);
-    ourRequest.onload = function () {
-        if (ourRequest.status >= 200 && ourRequest.status < 400) {//Handling Exceptions/Errors
-            var myData = JSON.parse(ourRequest.responseText);
-            var someContent = " ";
-            for (i = 0; i < myData.length; i++) {
-                someContent = "<ul>" + myData[i].title + "</ul>" + "<p> The Best Character in that movie was    " + myData[i].Casting.bestChar + "</p>";
-                theContainer[0].insertAdjacentHTML('beforeend', someContent);
-            }
-            console.log(myData);
-        } else {
-            console.log("There was an error requesting the file");
-        }
-    }
-    ourRequest.send();
-    clickCounter++;
-    if (clickCounter = 2) {
-        btn[0].style.display = "none";
-        theContainer[1].style.display = "none";
-    } else {
-        console.log("Something is wrong with the Button!");
-    }
-};
+// function theFunction() {
+//     var ourRequest = new XMLHttpRequest();
+//     ourRequest.open('GET', 'starWarsCast2.json', true);
+//     ourRequest.onload = function () {
+//         if (ourRequest.status >= 200 && ourRequest.status < 400) {//Handling Exceptions/Errors
+//             var myData = JSON.parse(ourRequest.responseText);
+//             var someContent = " ";
+//             for (i = 0; i < myData.length; i++) {
+//                 someContent = "<ul>" + myData[i].title + "</ul>" + "<p> The Best Character in that movie was    " + myData[i].Casting.bestChar + "</p>";
+//                 theContainer[0].insertAdjacentHTML('beforeend', someContent);
+//             }
+//             console.log(myData);
+//         } else {
+//             console.log("There was an error requesting the file");
+//         }
+//     }
+//     ourRequest.send();
+//     clickCounter++;
+//     if (clickCounter = 2) {
+//         btn[0].style.display = "none";
+//         theContainer[1].style.display = "none";
+//     } else {
+//         console.log("Something is wrong with the Button!");
+//     }
+// };
