bcce2c462a67125d1a5fab843e11ceb06a764dc8
rodriguezoswaldo <owner2303@gmail.com>
2019-06-17 22:29:09 -0400

    commit

--- animation/script.js
+++ animation/script.js
@@ -1,5 +1,9 @@
 anime({
-    targets: 'div.box.red',
+    targets: 'div.box',
+    skewY: [
+        {value:200, duration: 500},
+        {value:0, duration: 500}
+    ],
     translateY: [
         {value:200, duration: 500},
         {value:0, duration: 500}
@@ -7,41 +11,45 @@ anime({
     rotate: {
         value:'1turn',
         easing: 'easeInOutSine'
+    },
+    backgroundImage: "url('https://source.unsplash.com/random/100x104')",
+    delay: (el,i, l)=>{
+        return i*2000
     }
 });
-anime({
-    targets: 'div.box.blue',
-    translateY: [
-        {value:200, duration: 500, delay: 1000},
-        {value:0, duration: 500}
-    ],
-    rotate: {
-        value:'2turn',
-        easing: 'easeInOutSine',
-        delay:1000
-    }
-});
-anime({
-    targets: 'div.box.green',
-    translateY: [
-        {value:200, duration: 500, delay:2000},
-        {value:0, duration: 500}
-    ],
-    rotate: {
-        value:'3turn',
-        easing: 'easeInOutSine',
-        delay: 2000
-    }
-});
-anime({
-    targets: 'div.box.yellow',
-    translateY: [
-        {value:200, duration: 500, delay:3000},
-        {value:0, duration: 500}
-    ],
-    rotate: {
-        value:'4turn',
-        easing: 'easeInOutSine',
-        delay: 3000
-    }
-});
+// anime({
+//     targets: 'div.box.blue',
+//     translateY: [
+//         {value:200, duration: 500, delay: 1000},
+//         {value:0, duration: 500}
+//     ],
+//     rotate: {
+//         value:'2turn',
+//         easing: 'easeInOutSine',
+//         delay:1000
+//     }
+// });
+// anime({
+//     targets: 'div.box.green',
+//     translateY: [
+//         {value:200, duration: 500, delay:2000},
+//         {value:0, duration: 500}
+//     ],
+//     rotate: {
+//         value:'3turn',
+//         easing: 'easeInOutSine',
+//         delay: 2000
+//     }
+// });
+// anime({
+//     targets: 'div.box.yellow',
+//     translateY: [
+//         {value:200, duration: 500, delay:3000},
+//         {value:0, duration: 500}
+//     ],
+//     rotate: {
+//         value:'4turn',
+//         easing: 'easeInOutSine',
+//         delay: 3000
+//     }
+// });
